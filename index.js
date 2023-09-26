b310abe82c84c82dfdcf5e49bae68e5e857c0445
Farid Lopez <faridlopez5@gmail.com>
2023-09-25 17:37:03 -0700

    updated file for if/else

---
+++ index.js
@@ -0,0 +1,41 @@
+//Make a JS code that does the following 
+// If the value is truthy, print true
+// If the value is falsy, print the other results.
+
+/*let word= "truthy";
+
+if (word != "falsy") {console.log("true")} 
+else { console.log("The boolean value false is falsy")}
+*/
+
+//Use if/else statements
+// code sum of two numbers and prints the result below
+// 101 is greater than 100
+
+/*let num1 = 50; 
+
+let num2 = 51;
+
+if ( num1 + num2 == 101) {
+    console.log("101 is greater than 100")
+}
+*/
+
+// Use if/else to print true if both nums are greater or equal to 5
+
+/*let num1=5;
+let num2=6;
+
+if (num1 + num2 >= 5) {console.log(true)} 
+
+else {console.log(false)}
+*/
+
+
+//use operators to compare two values
+// if one of values is truthy then print true
+// else print false
+
+
+
+
